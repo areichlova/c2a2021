1029ba24bceaf8857b4a1b68e3bd163196748d94
areic <areic@10.1.90.22>
2021-05-17 09:45:47 +0200

    Druhy ctverec

--- MujProjekt/src/Main.java
+++ MujProjekt/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		Ctverec c = new Ctverec(5);
 		System.out.println(c);
+		Ctverec c2 = new Ctverec(15);
+		System.out.println(c2);
 	}
 
 }
